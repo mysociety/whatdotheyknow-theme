8d736627a568053eae792fa1e74a2f026a081af3
Robin Houston <robin.houston@gmail.com>
2012-01-05 19:10:32 +0000

    revert for 0.3

--- uninstall.rb
+++ uninstall.rb
@@ -1,1 +1,8 @@
 # Uninstall hook code here
+
+main_app_path = File.join(RAILS_ROOT, 'public', 'alavetelitheme')
+if File.exists?(main_app_path) && File.symlink?(main_app_path)
+	print "Deleting symbolink link at #{main_app_path}... "
+	File.delete(main_app_path)
+	puts "done"
+end
