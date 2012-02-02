3c69cfcef9f9bc0b8d0defce0cc16a005c46205d
Seb Bacon <seb.bacon@gmail.com>
2012-02-02 12:23:48 +0000

    Move WDTK's categories into WDTK's theme (rather than having to edit them in core Alaveteli)

--- lib/alavetelitheme.rb
+++ lib/alavetelitheme.rb
@@ -7,3 +7,12 @@ class ActionController::Base
         self.prepend_view_path File.join(File.dirname(__FILE__), "views")
     end
 end
+
+# In order to have the theme lib/ folder ahead of the main app one,
+# inspired in Ruby Guides explanation: http://guides.rubyonrails.org/plugins.html
+%w{ . }.each do |dir|
+  path = File.join(File.dirname(__FILE__), dir)
+  $LOAD_PATH.insert(0, path)
+  ActiveSupport::Dependencies.autoload_paths << path
+  ActiveSupport::Dependencies.autoload_once_paths.delete(path)
+end
