8ba1a9518e200ecb042b820ea9d075ac82e31f36
Robin Houston <robin.houston@gmail.com>
2012-02-19 22:58:54 +0000

    Simplify the ActionMailer view_paths hackery
    It wasn’t working on the live site, possibly because it conflicted
    with the hackery in the exception_notification plugin.

--- lib/patch_mailer_paths.rb
+++ lib/patch_mailer_paths.rb
@@ -4,41 +4,8 @@
 #
 require 'dispatcher'
 Dispatcher.to_prepare do
-    # See http://www.quirkey.com/blog/2008/08/28/actionmailer-hacking-multiple-template-paths/
-    # and #comment-59867 thereon.
-    ActionMailer::Base.class_eval do
-        class_inheritable_accessor :view_paths
-    
-        def self.prepend_view_path(path)
-          if view_paths[0] != path
-              self.view_paths = [path] + self.view_paths
-          end
-        end
-
-        def self.append_view_path(path)
-          if view_paths[-1] != path
-              self.view_paths = self.view_paths + [path]
-          end
-        end
-
-        def view_paths
-          self.class.view_paths
-        end
-
-        private
-        def self.view_paths
-          @@view_paths||=ActionController::Base.view_paths
-        end
-    
-        def initialize_template_class_without_helper(assigns)
-          template = ActionView::Base.new(view_paths, assigns, self)
-          template.template_format = default_template_format
-          template
-        end
-
-    end
-    
     # Override mailer templates with theme ones.
-    ActionMailer::Base.prepend_view_path File.join(File.dirname(__FILE__), "views")
+    ActionMailer::Base.view_paths.unshift File.join(File.dirname(__FILE__), "views")
+    ActionMailer::Base.view_paths = ActionView::Base.process_view_paths(ActionMailer::Base.view_paths)
     Rails.logger.debug "view_paths: #{ActionMailer::Base.view_paths.inspect}"
 end
