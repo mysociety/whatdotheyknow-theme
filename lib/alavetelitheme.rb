mysociety/whatdotheyknow-theme

commit 9cedf59d2560131b13f9b3fb642490a7be82a1ac

Make the theme compatible with the new feature in Alaveteli that lets us install multiple themes, by ensuring important filenames and method names don't clash

lib/alavetelitheme.rb
@@ -1,9 +1,9 @@
 require 'routingfilter'
 
 class ActionController::Base
-    before_filter :set_view_paths
+    before_filter :set_whatdotheyknow_view_paths
 
-    def set_view_paths
+    def set_whatdotheyknow_view_paths
         self.prepend_view_path File.join(File.dirname(__FILE__), "views")
     end
 end
@@ -18,13 +18,13 @@ end
 end
 
 # Monkey patch app code
-require 'patch_mailer_paths.rb'
-require 'controller_patches.rb'
-require 'model_patches.rb'
-require 'helper_patches.rb'
+for patch in ['patch_mailer_paths.rb',
+              'controller_patches.rb',
+              'model_patches.rb',
+              'helper_patches.rb',
+              'config/custom-routes.rb',
+              'gettext_setup.rb',
+              'controller_patches.rb']
+    require File.expand_path "../#{patch}", __FILE__
+end
 
-# Extend routes
-require 'config/custom-routes.rb'
-
-# Plug theme-specific locale strings
-require 'gettext_setup.rb'

commit 82b3203dce2edbc5d4523b61c9d36fee3e56a50a

Style error pages correctly
Fix a regression introduced by 9cedf59d2560131b13f9b3fb642490a7be82a1ac.

lib/alavetelitheme.rb
@@ -6,6 +6,13 @@ class ActionController::Base
     def set_whatdotheyknow_view_paths
         self.prepend_view_path File.join(File.dirname(__FILE__), "views")
     end
+    
+    # Note that set_view_paths is called by Alaveteli from the
+    # rescue_action_in_public method, in order that error pages
+    # may be themed correctly. Since whatdotheyknow-theme is a
+    # primary theme that ought to style error pages, we define
+    # this as an alias
+    alias :set_whatdotheyknow_view_paths, :set_view_paths
 end
 
 # In order to have the theme lib/ folder ahead of the main app one,