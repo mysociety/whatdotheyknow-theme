3d4bcbc61c8f7a447293a3fffcb873f289b4b393
Louise Crow <louise.crow@gmail.com>
2012-08-14 16:57:18 +0100

    None of these rules are global now, so no need to set the global flag.

--- lib/censor_rules.rb
+++ lib/censor_rules.rb
@@ -35,7 +35,6 @@ rules_data.each do |d|
     rule = CensorRule.regexps.find_by_text(d[:text])
     if rule.nil?
         new_rule = CensorRule.new(d)
-        new_rule.allow_global = true
         new_rule.save!
     end
 end
