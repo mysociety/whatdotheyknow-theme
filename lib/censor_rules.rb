5edb0b74abfc6e671a857a7a6f0f034e5bfbc267
Louise Crow <louise.crow@gmail.com>
2012-08-14 11:52:48 +0100

    Make censor rules that only need to work on one request request-specific.

--- lib/censor_rules.rb
+++ lib/censor_rules.rb
@@ -1,18 +1,21 @@
 rules_data = [{:text => '\*\*\*+\nPolly Tucker.*',
                   :replacement => '',
                   :regexp => true,
+                  :info_request => InfoRequest.find_by_url_name('total_number_of_objects_in_the_n_6')
                   :last_edit_editor => 'system',
                   :last_edit_comment => 'Refactored from remove_privacy_sensitive_things!'},
 
               {:text => 'Andy 079.*',
                   :replacement => 'Andy [mobile number]',
                   :regexp => true,
+                  :info_request => InfoRequest.find_by_url_name('cctv_data_retention_and_use')
                   :last_edit_editor => 'system',
                   :last_edit_comment => 'Refactored from remove_privacy_sensitive_things!'},
 
               {:text => '.Complaints and Corporate Affairs Officer.,\s+Westminster Primary Care Trust.+',
                   :replacement => 'Andy [mobile number]',
                   :regexp => true,
+                  :info_request => InfoRequest.find_by_url_name('how_do_the_pct_deal_with_retirin_113')
                   :last_edit_editor => 'system',
                   :last_edit_comment => 'Refactored from remove_privacy_sensitive_things!'},
 
