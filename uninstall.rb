0d63c3395236cd073a914c7ef3e420d041bd3d56
Louise Crow <louise.crow@gmail.com>
2013-12-04 13:37:15 +0000

    No need to unsymlink
    Superceded by asset pipeline

--- uninstall.rb
+++ uninstall.rb
@@ -1,8 +1,1 @@
 # Uninstall hook code here
-
-main_app_path = Rails.root.join('public', 'alavetelitheme')
-if File.exists?(main_app_path) && File.symlink?(main_app_path)
-	print "Deleting symbolic link at #{main_app_path}... "
-	File.delete(main_app_path)
-	puts "done"
-end
