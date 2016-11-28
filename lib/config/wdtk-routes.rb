6da73596c4ecbd30a415490aab572964980fc943
Gareth Rees <gareth@garethrees.co.uk>
2016-11-28 15:29:00 +0000

    Add HTTP methods to custom routes
    The survey POSTs a non-alaveteli endpoint so shouldn't need POST.

--- lib/config/wdtk-routes.rb
+++ lib/config/wdtk-routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   # Add a route for the survey
   scope '/profile/survey' do
     root :to => 'user#survey', :as => :survey
-    match '/reset' => 'user#survey_reset', :as => :survey_reset
+    get '/reset' => 'user#survey_reset', :as => :survey_reset
   end
 
-  match "/help/ico-guidance-for-authorities" => redirect("https://ico.org.uk/media/for-organisations/documents/how-to-disclose-information-safely-removing-personal-data-from-information-requests-and-datasets/1432979/how-to-disclose-information-safely.pdf
+  get "/help/ico-guidance-for-authorities" => redirect("https://ico.org.uk/media/for-organisations/documents/how-to-disclose-information-safely-removing-personal-data-from-information-requests-and-datasets/1432979/how-to-disclose-information-safely.pdf
   "),
   :as => :ico_guidance
 end
