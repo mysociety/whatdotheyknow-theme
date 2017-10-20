345ca0b9b4cac361840a942bc285743887aff9ec
Louise Crow <louise.crow@gmail.com>
2017-10-20 17:54:12 +0100

    2-spaces

--- lib/controller_patches.rb
+++ lib/controller_patches.rb
@@ -1,18 +1,17 @@
 Rails.configuration.to_prepare do
-    UserController.class_eval do
-        require 'survey'
+  UserController.class_eval do
+    require 'survey'
 
-        def survey
-        end
+    def survey
+    end
 
-        # Reset the state of the survey so it can be answered again.
-        # Handy for testing; not allowed in production.
-        def survey_reset
-            raise "Not allowed in production" if ENV["RAILS_ENV"] == "production"
-            raise "Not logged in" if !@user
-            @user.survey.allow_new_survey
-            return redirect_to survey_url
-        end
+    # Reset the state of the survey so it can be answered again.
+    # Handy for testing; not allowed in production.
+    def survey_reset
+      raise "Not allowed in production" if ENV["RAILS_ENV"] == "production"
+      raise "Not logged in" if !@user
+      @user.survey.allow_new_survey
+      return redirect_to survey_url
     end
   end
 
