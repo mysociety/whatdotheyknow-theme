636b881b6e66a4d0177a48f4a819f4ac8275bd5d
Zarino Zappia <mail@zarino.co.uk>
2019-07-31 14:59:27 +0100

    Google Optimize experiment for donation button wording
    Google is discontinuing Analytics Content Experiments, so here we set
    up a Google Optimize experiment on the request page.
    
    GTM-53XVLCX is the Optimize Container ID for whatdotheyknow.com and
    XinKUWDjTuK7Yxyo3ZBS4g is the ID of the Experiment we’ve set up.

--- app/assets/javascripts/tests.js
+++ app/assets/javascripts/tests.js
@@ -1,10 +1,42 @@
-(function($) {
-  $(function () {
-    window.cxVariation = cxApi.chooseVariation();
-    if(window.cxVariation === 1){
-      // Do something.
+// https://support.google.com/optimize/answer/9059383
+function gtag() { dataLayer.push(arguments); };
+
+function donateButtonTextVariant(variant) {
+  if (variant === '0') {
+      $(document).ready(function(){
+        $('.donate-cta__button').text('Donate now');
+      });
+  } else if (variant === '1') {
+    $(document).ready(function(){
+      $('.donate-cta__button').text('Donate £5 now');
+    });
+  } else if (variant === '2') {
+    $(document).ready(function(){
+      $('.donate-cta__button').text('Donate £10 now');
+    });
+  }
+};
+
+gtag('event', 'optimize.callback', {
+  name: 'XinKUWDjTuK7Yxyo3ZBS4g',
+  callback: donateButtonTextVariant
+});
+
+$(function(){
+  // Record an event whenever the donation button is clicked.
+  $('.donate-cta__button').on('click', function(){
+    if( typeof ga !== 'undefined' && ga.loaded ){
+      e.preventDefault();
+      var href = $(this).attr('href');
+      ga('send', {
+        hitType: 'event',
+        eventCategory: 'Request page donate button',
+        eventAction: 'click',
+        eventLabel: document.title,
+        hitCallback: function() {
+          window.location.href = href;
+        }
+      });
     }
   });
-})(window.jQuery);
-
-
+});
