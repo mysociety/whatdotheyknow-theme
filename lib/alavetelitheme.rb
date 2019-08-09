5ae244747345d2c6ebec18e920df5d769d02a69d
Liz Conlan <liz@mysociety.org>
2019-08-09 11:15:59 +0100

    Define filters with before_action instead of before_filter
    before_filter was deprecated in Rails 4.2 and will be removed in 5.1
    
    https://edgeguides.rubyonrails.org/4_2_release_notes.html#action-pack-notable-changes

--- lib/alavetelitheme.rb
+++ lib/alavetelitheme.rb
@@ -1,7 +1,7 @@
 require 'routingfilter'
 
 class ActionController::Base
-    before_filter :set_whatdotheyknow_view_paths
+    before_action :set_whatdotheyknow_view_paths
 
     def set_whatdotheyknow_view_paths
         self.prepend_view_path File.join(File.dirname(__FILE__), "views")
