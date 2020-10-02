f482a199f82802ba8a8c527f73484a019eefa41f
Martyn Dewar <249418+mdeuk@users.noreply.github.com>
2020-10-02 17:13:40 +0100

    Update wdtk-routes.rb to add redirects for cymru, scotland, and wales
    Based on #417 - adds redirects for cymru, scotland, and wales to appropriate tags.

--- lib/config/wdtk-routes.rb
+++ lib/config/wdtk-routes.rb
@@ -2,6 +2,9 @@
 
 Rails.application.routes.draw do
   get '/london' => redirect('/body?tag=london', status: 302)
+  get '/scotland' => redirect('/body?tag=scotland', status: 302)
+  get '/cymru' => redirect('/body?tag=wales', status: 302)
+  get '/wales' => redirect('/body?tag=wales', status: 302)
 
   # Add a route for the survey
   scope '/profile/survey' do
