c9ba02707e6d6874235ae7c215adbe7bce5edb21
Graeme Porteous <graeme@rgbp.co.uk>
2021-03-30 13:09:12 +0100

    Merge branch 'add-alaveteli-pro-translation-repo'

--- lib/alavetelitheme.rb
+++ lib/alavetelitheme.rb
@@ -71,9 +71,11 @@ $alaveteli_route_extensions << 'wdtk-routes.rb'
 # Tell FastGettext about the theme's translations: look in the theme's
 # locale-theme directory for a translation in the first place, and if
 # it isn't found, look in the Alaveteli locale directory next:
-repos = [
-    FastGettext::TranslationRepository.build('app', :path=>File.join(File.dirname(__FILE__), '..', 'locale-theme'), :type => :po),
-    FastGettext::TranslationRepository.build('app', :path=>'locale', :type => :po)
-]
-FastGettext.add_text_domain 'app', :type=>:chain, :chain=>repos
-FastGettext.default_text_domain = 'app'
+paths = []
+paths << File.join(File.dirname(__FILE__), '..', 'locale-theme')
+paths << 'locale_alaveteli_pro' if AlaveteliConfiguration::enable_alaveteli_pro
+paths << 'locale'
+repos = paths.map do |path|
+  FastGettext::TranslationRepository.build('app', :path => path, :type => :po)
+end
+AlaveteliLocalization.set_default_text_domain('app', repos)
