20eec3f85fff29c669f7c62acf27b4c644741f85
Gareth Rees <gareth@garethrees.co.uk>
2021-05-17 17:07:35 +0100

    Refactor wdtk-routes
    * Remove unnecessary `via` option. Implied via `get` route helper
    * Ruby 1.9 symbols
    * Symbol over String named routes

--- lib/config/wdtk-routes.rb
+++ lib/config/wdtk-routes.rb
@@ -11,38 +11,31 @@ Rails.application.routes.draw do
 
   # Add a route for the survey
   scope '/profile/survey' do
-    root :to => 'user#survey', :as => :survey
-    get '/reset' => 'user#survey_reset', :as => :survey_reset
+    root to: 'user#survey', as: :survey
+    get '/reset' => 'user#survey_reset', as: :survey_reset
   end
 
   get "/help/ico-guidance-for-authorities" => redirect("https://ico.org.uk/media/for-organisations/documents/how-to-disclose-information-safely-removing-personal-data-from-information-requests-and-datasets/2013958/how-to-disclose-information-safely.pdf"),
-  :as => :ico_guidance
+      as: :ico_guidance
 
-  get '/help/principles'  => 'help#principles',
-    :via => 'get',
-    :as => 'help_principles'
+  get '/help/principles' => 'help#principles',
+      as: :help_principles
 
-  get '/help/house_rules'  => 'help#house_rules',
-    :via => 'get',
-    :as => 'help_house_rules'
+  get '/help/house_rules' => 'help#house_rules',
+      as: :help_house_rules
 
-  get '/help/how'  => 'help#how',
-    :via => 'get',
-    :as => 'help_how'
+  get '/help/how' => 'help#how',
+      as: :help_how
 
-  get '/help/complaints'  => 'help#complaints',
-    :via => 'get',
-    :as => 'help_complaints'
+  get '/help/complaints' => 'help#complaints',
+      as: :help_complaints
 
-  get '/help/volunteers'  => 'help#volunteers',
-    :via => 'get',
-    :as => 'help_volunteers'
+  get '/help/volunteers' => 'help#volunteers',
+      as: :help_volunteers
 
-  get '/help/beginners'  => 'help#beginners',
-    :via => 'get',
-    :as => 'help_beginners'
+  get '/help/beginners' => 'help#beginners',
+      as: :help_beginners
 
-  get '/help/ico_officers'  => 'help#ico_officers',
-    :via => 'get',
-    :as => 'help_ico_officers'
+  get '/help/ico_officers' => 'help#ico_officers',
+      as: :help_ico_officers
 end
