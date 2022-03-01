d136d1b41e794bac05003a66e12543593d78e25e
sallytay <88660019+sallytay@users.noreply.github.com>
2022-03-01 14:08:57 +0000

    Add no reply address for The Adjudicator's Office
    When information is sent via the file transfer service it comes from a no reply email address. This address then gets picked up as the reply address for WhatDoTheyKnow users creating a bounce message rather than going through to the authority

--- lib/model_patches.rb
+++ lib/model_patches.rb
@@ -117,6 +117,7 @@ Rails.configuration.to_prepare do
     no-reply@somersetwestandtaunton.gov.uk
     csfinanceplanning&performance.briefingteam@hmrc.gov.uk
     foi.foi@lincs.police.uk
+    microsoftoffice365@messaging.microsoft.com
   )
 
   User::EmailAlerts.instance_eval do
