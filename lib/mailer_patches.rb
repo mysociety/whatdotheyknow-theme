aeb99d45f8b6aa8b6f35a92edda5decfb09e1b04
Graeme Porteous <graeme@rgbp.co.uk>
2023-04-19 13:02:57 +0100

    Add link survey to track mailer
    Link to the mySociety 20th anniversary How have you used WDTK survey.

--- lib/mailer_patches.rb
+++ lib/mailer_patches.rb
@@ -3,4 +3,12 @@ Rails.configuration.to_prepare do
   ContactMailer.class_eval do
     prepend VolunteerContactForm::MailerMethods
   end
+
+  TrackMailer.class_eval do
+    def mail(*args)
+      super(*args) do |format|
+        format.text { render 'event_digest_with_survey' }
+      end
+    end
+  end
 end
