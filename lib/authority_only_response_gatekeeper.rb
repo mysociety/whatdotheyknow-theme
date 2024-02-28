42c44160e6c05c91bdd0e5f38f6307996e543f5c
Graeme Porteous <graeme@rgbp.co.uk>
2024-02-28 09:44:56 +0000

    Patch authority only response gatekeeper
    Accept responses to authority only requests from extra addresses which
    authorities might use. For example no-reply addresses from case
    management systems which aren't changeable.

---
+++ lib/authority_only_response_gatekeeper.rb
@@ -0,0 +1,16 @@
+module AuthorityOnlyResponseGatekeeper
+  EXTRA_ALLOWED_EMAILS = [
+    { public_body_id: 27, email: 'no-reply@cabinetoffice.ecase.co.uk' }
+  ]
+
+  def allow?(mail)
+    public_body_id = info_request.public_body_id
+    email = MailHandler.get_from_address(mail)
+
+    return true if EXTRA_ALLOWED_EMAILS.any? do |a|
+      a[:public_body_id] == public_body_id && a[:email] == email
+    end
+
+    super
+  end
+end
