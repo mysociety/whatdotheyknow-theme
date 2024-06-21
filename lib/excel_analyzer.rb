4e734fe4718eedf393b72210853ff53158f850b7
Graeme Porteous <graeme@rgbp.co.uk>
2024-06-21 14:23:39 +0100

    [ExcelAnalyzer] remove prominence contact us link
    The ProminenceHelper automatically injects a contact us link into the
    reason why an object has been hidden. While normally this is okay we
    want to remove this for files hidden by ExcelAnalyzer to reduce inbox
    support.

--- lib/excel_analyzer.rb
+++ lib/excel_analyzer.rb
@@ -8,6 +8,10 @@ Rails.application.config.before_initialize do
   loader.inflector.inflect("pii_badger_job" => "PIIBadgerJob")
 end
 
+ExcelAnalyzer::PROMINENCE_REASON = "We've found a problem with this file, so " \
+  "it's been hidden while we review it. We might not be able to give more " \
+  "details until then."
+
 ExcelAnalyzer.on_hidden_metadata = ->(attachment_blob, _) do
   foi_attachment = FoiAttachment.joins(:file_blob).
     find_by(active_storage_blobs: { id: attachment_blob })
@@ -17,9 +21,7 @@ ExcelAnalyzer.on_hidden_metadata = ->(attachment_blob, _) do
 
   foi_attachment.update_and_log_event(
     prominence: 'hidden',
-    prominence_reason: "We've found a problem with this file, so it's been " \
-                       "hidden while we review it. We might not be able to " \
-                       "give more details until then.",
+    prominence_reason: ExcelAnalyzer::PROMINENCE_REASON,
     event: {
       editor: User.internal_admin_user,
       reason: 'ExcelAnalyzer: hidden data detected'
