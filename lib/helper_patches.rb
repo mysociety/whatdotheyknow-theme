246e7353aa94ae361527f9293d6106bd99286694
Graeme Porteous <graeme@rgbp.co.uk>
2024-11-22 09:47:04 +0000

    Migrate Stripe Plans to Prices
    Now core allows multiple prices we can handle alternative price
    programmatically too.

--- lib/helper_patches.rb
+++ lib/helper_patches.rb
@@ -31,4 +31,30 @@ Rails.configuration.to_prepare do
 
     prepend ExcelAnalyzerDisableContactUs
   end
+
+  AlaveteliPro::PriceHelper.class_eval do
+    module AlternativePriceText
+      def short_alternative_price_text(price)
+        amount = format_currency(
+          price.unit_amount_with_tax, no_cents_if_whole: true
+        )
+
+        if interval(price) == 'day' && interval_count(price) == 1
+          _('or {{amount}} daily', amount: amount)
+        elsif interval(price) == 'week' && interval_count(price) == 1
+          _('or {{amount}} weekly', amount: amount)
+        elsif interval(price) == 'month' && interval_count(price) == 1
+          _('or {{amount}} monthly', amount: amount)
+        elsif interval(price) == 'year' && interval_count(price) == 1
+          _('or {{amount}} annually', amount: amount)
+        else
+          _('or {{amount}} every {{interval}}',
+            amount: amount,
+            interval: pluralize_interval(price))
+        end
+      end
+    end
+
+    prepend AlternativePriceText
+  end
 end
