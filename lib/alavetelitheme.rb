4465c80218d90eef77747268f82074fde0e80441
Graeme Porteous <graeme@rgbp.co.uk>
2025-06-03 14:46:00 +0100

    Minor cleanup
    - Style/RedundantSelf
    - Style/Alias - alias methods without symbols
    - Style/ColonMethodCall - don't use colon method calls
    - Style/HashSyntax
    - Remove constant

--- lib/alavetelitheme.rb
+++ lib/alavetelitheme.rb
@@ -2,7 +2,7 @@ class ActionController::Base
   before_action :set_whatdotheyknow_view_paths
 
   def set_whatdotheyknow_view_paths
-    self.prepend_view_path File.join(File.dirname(__FILE__), "views")
+    prepend_view_path File.join(File.dirname(__FILE__), "views")
   end
 
   # Note that set_view_paths is called by Alaveteli from the
@@ -10,7 +10,7 @@ class ActionController::Base
   # may be themed correctly. Since whatdotheyknow-theme is a
   # primary theme that ought to style error pages, we define
   # this as an alias
-  alias :set_view_paths :set_whatdotheyknow_view_paths
+  alias set_view_paths set_whatdotheyknow_view_paths
 end
 
 # Append individual theme assets to the asset path
@@ -20,12 +20,12 @@ theme_asset_path = File.join(File.dirname(__FILE__),
                              'assets')
 theme_asset_path = Pathname.new(theme_asset_path).cleanpath.to_s
 
-LOOSE_THEME_ASSETS = lambda do |logical_path, filename|
+loose_theme_assets = lambda do |logical_path, filename|
   filename.start_with?(theme_asset_path) &&
   !['.js', '.css', ''].include?(File.extname(logical_path))
 end
 
-Rails.application.config.assets.precompile.unshift(LOOSE_THEME_ASSETS)
+Rails.application.config.assets.precompile.unshift(loose_theme_assets)
 
 Rails.application.config.assets.precompile << ["tests.js"]
 
@@ -71,9 +71,9 @@ $alaveteli_route_extensions << 'wdtk-routes.rb'
 # it isn't found, look in the Alaveteli locale directory next:
 paths = []
 paths << File.join(File.dirname(__FILE__), '..', 'locale-theme')
-paths << 'locale_alaveteli_pro' if AlaveteliConfiguration::enable_alaveteli_pro
+paths << 'locale_alaveteli_pro' if AlaveteliConfiguration.enable_alaveteli_pro
 paths << 'locale'
 repos = paths.map do |path|
-  FastGettext::TranslationRepository.build('app', :path => path, :type => :po)
+  FastGettext::TranslationRepository.build('app', path: path, type: :po)
 end
 AlaveteliLocalization.set_default_text_domain('app', repos)
