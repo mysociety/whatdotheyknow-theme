53e5af239f9403c4f5b7d1e575eca1699ae718cd
FOIMonkey <FOIMonkey@users.noreply.github.com>
2026-04-10 12:51:31 +0100

    Adding lambeth do not reply

--- lib/model_patches.rb
+++ lib/model_patches.rb
@@ -268,6 +268,7 @@ Rails.configuration.to_prepare do
     do-not-reply=midlothian.gov.uk@email.firmstep.com
     do-not-reply@midlothian.gov.uk
     FOIenquiries=aberdeencity.gov.uk@email.firmstep.com
+    icaseworkinformationrequests@lambeth.gov.uk
   )
 
   User.content_limits = {
